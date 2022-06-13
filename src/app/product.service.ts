492d4c32d3544c2177afd88d2dba2784d00d161a
AIsacson <aisacson@kth.se>
2022-06-13 21:45:10 +0200

    part 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productUrl = '../assets/products.json';
 
   constructor(private _http: HttpClient) {
   }
 
   getAlbum (_id: number): Observable<Album> {
     return this._http.get<Album>(this._albumUrl);
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get<Product[]>(this._productUrl);
   }
 }
